6049c48f6112cec5994fee0e819b9ab7447024f0
singalasreenivasulu55 <singalasreenivasulu55@gmail.com>
2024-01-24 16:47:19 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -8,28 +8,29 @@ N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-       echo -e "ERROR:: $2 ...$R FAILED $N"
-       exit 1
-    else 
-       echo -e "$2 ...$G SUCCESS $N"
+    then
+        echo -e "ERROR:: $2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-  echo -e "$R ERROR:: please run this script with root access $N"
-  exit 1 # you can give other than 0
-else 
-  echo "You are root user"
-fi # fi means reverse of if , indicating condition end
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-yum install mysqll -y &>> $LOGFILE
+yum install mysql -y &>> $LOGFILE
 
-VALIDATE $? "Installing MYSQL" 
+VALIDATE $? "Installing MySQL"
 
 yum install git -y &>> $LOGFILE
 
-VALIDATE $? "Installing GIT" 
+VALIDATE $? "Installing GIT"
