3ef7473e2ec36fd8fe31bba5eea3fec39d41a7d9
singalasreenivasulu55 <singalasreenivasulu55@gmail.com>
2024-02-01 12:26:11 +0530

    added 2nd script

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -14,7 +14,7 @@ fi
 
 FILE_TO_DELETE=$(find . -type f -mtime +14 -name "*.log")
 
-while IFS read -r line 
+while IFS= read -r line 
 do 
    echo "Deleting files: $LINE"
 done <<< $FILES_TO_DELETE
